015343ed0ac76e23fdcb2640431e39d08756ab3a
RoseJane <rjane@thousandminds.com>
2015-06-10 18:19:51 +0800

    Added Registration form layout (all elements/fields included)

--- app/src/main/java/com/brewedconcepts/hoppler/RegistrationActivity.java
+++ app/src/main/java/com/brewedconcepts/hoppler/RegistrationActivity.java
@@ -1,7 +1,17 @@
 package com.brewedconcepts.hoppler;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
 /**
  * Created by RoseJane on 6/9/2015.
  */
-public class RegistrationActivity {
+public class RegistrationActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_layout);
+
+
+    }
 }
