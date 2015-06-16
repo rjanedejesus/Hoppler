3108d459f2d1963360fe8b321d3f0598efa4b564
RoseJane <rjane@thousandminds.com>
2015-06-16 15:54:06 +0800

    Created new login page (June 16, 2015) - part 4

--- app/src/main/java/com/brewedconcepts/hoppler/LoginScreen.java
+++ app/src/main/java/com/brewedconcepts/hoppler/LoginScreen.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -19,11 +20,12 @@ public class LoginScreen extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        cache = Cache.open(Cache.CACHE_USER);
-        setContentView(R.layout.login_layout);
-       // getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setDisplayShowHomeEnabled(false);
+
         ActionBar bar = getActionBar();
+        setContentView(R.layout.login_layout);
+        getActionBar().setHomeButtonEnabled(true);
+        getActionBar().setDisplayShowHomeEnabled(true);
+        getActionBar().setIcon(R.drawable.btn_nav_menu);
         bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#007EFE")));
     }
 
@@ -32,7 +34,7 @@ public class LoginScreen extends Activity {
         // Inflate the menu items for use in the action bar
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.login_activity_actions, menu);
-        return super.onCreateOptionsMenu(menu);
+        return true;
     }
 
 
